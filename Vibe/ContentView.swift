ac5d0c6908ac11b6b306b184422364006d6c3600
Tom Johnson <tom@beardedhen.com>
2022-05-24 14:37:50 +0100

    Initial Commit

---
+++ Vibe/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Vibe
+//
+//  Created by Tom Johnson on 24/05/2022.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Vibe/VibeApp.swift
@@ -0,0 +1,17 @@
+//
+//  VibeApp.swift
+//  Vibe
+//
+//  Created by Tom Johnson on 24/05/2022.
+//
+
+import SwiftUI
+
+@main
+struct VibeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
